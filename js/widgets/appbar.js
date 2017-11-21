0c1df7bcdc95db41c641d761e90ecae8be62b54f
Peter Lustig <admin@hrvy.ml>
2017-11-21 10:24:11 +0100

    Revert "Close pullbar when clicking somewhere -> needed for SPA"
    This reverts commit de45eb1e59ccbf1310228ead587472cdea624a95.

--- js/widgets/appbar.js
+++ js/widgets/appbar.js
@@ -381,8 +381,6 @@
                             $(that.pullMenu).show();
                             $(that.pullMenu).find(".app-bar-pullmenubar")
                                     .hide().not(".hidden").slideDown("fast");
-                            e.preventDefault();
-                            e.stopPropagation();
                         } else {
                             $(that.pullMenu).find(".app-bar-pullmenubar")
                                     .not(".hidden").show().slideUp("fast", function () {
